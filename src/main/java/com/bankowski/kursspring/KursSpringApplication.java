dc854d66bfeed795e86a5547000fe4d5c576d452
przemek <przeban3354@wp.pl>
2019-11-16 13:17:38 +0100

    [add] - some info

--- src/main/java/com/bankowski/kursspring/KursSpringApplication.java
+++ src/main/java/com/bankowski/kursspring/KursSpringApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KursSpringApplication {
-
+    // this method runs Spring container
     public static void main(String[] args) {
         SpringApplication.run(KursSpringApplication.class, args);
     }
